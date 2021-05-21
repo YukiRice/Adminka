a00b77c9e45b613d1afdde8439bc11a4257df8fd
YukiRice <81970044+YukiRice@users.noreply.github.com>
2021-05-21 11:49:39 +0700

    Update Adminka Binder.lua

--- Adminka Binder.lua
+++ Adminka Binder.lua
@@ -154,8 +154,8 @@ imgui.EndTooltip() end end
 
 
 update_state = false
-local script_vers = 1
-local script_vers_text = "21.05.2021"
+local script_vers = 2
+local script_vers_text = "22.05.2021"
 
 local update_url = "https://raw.githubusercontent.com/YukiRice/Adminka/main/update.ini" -- тут тоже свою ссылку
 local update_path = getWorkingDirectory() .. "/update.ini" -- и тут свою ссылку
